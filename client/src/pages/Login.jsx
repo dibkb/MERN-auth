d32e0b6c804dff198ab53eee2ef573d21dd23ec9
dibkb <dibas9110@gmail.con>
2022-11-20 20:31:56 +0530

    server setup

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -1,7 +1,23 @@
 import React from "react";
-
+import { Link } from "react-router-dom";
 const Login = () => {
-  return <div>Login</div>;
+  return (
+    <div>
+      <h3>Login</h3>
+      <form
+        style={{
+          display: "flex",
+          flexDirection: "column",
+          rowGap: "1rem",
+        }}
+      >
+        <input type="text" placeholder="Email" />
+        <input type="password" placeholder="Password" />
+        <button>Login</button>
+        <Link to="/register">Register</Link>
+      </form>
+    </div>
+  );
 };
 
 export default Login;
--- client/src/pages/Register.jsx
+++ client/src/pages/Register.jsx
@@ -1,7 +1,24 @@
 import React from "react";
-
+import { Link } from "react-router-dom";
 const Register = () => {
-  return <div>Register</div>;
+  return (
+    <div>
+      <h3>Register</h3>
+      <form
+        style={{
+          display: "flex",
+          flexDirection: "column",
+          rowGap: "1rem",
+        }}
+      >
+        <input type="text" placeholder="Name" />
+        <input type="text" placeholder="Email" />
+        <input type="password" placeholder="Password" />
+        <button>Register</button>
+        <Link to="/login">Login</Link>
+      </form>
+    </div>
+  );
 };
 
 export default Register;
