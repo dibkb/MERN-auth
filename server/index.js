f71a05e09c12862cc9b597534129c9eb74d972e7
dibkb <dibas9110@gmail.con>
2022-11-20 21:06:45 +0530

    env setup and mongo connection

--- server/index.js
+++ server/index.js
@@ -1,1 +1,21 @@
+import express from "express";
+import mongoose from "mongoose";
+import * as dotenv from "dotenv";
+dotenv.config();
+const app = express();
+const PORT = process.env.PORT;
+// middleware
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+// mongoose connect
+mongoose
+  .connect(process.env.MONG0_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then((res) => console.log(`Connection Succesful`))
+  .catch((err) => console.log(`Error in DB connection ${err}`));
 
+app.listen(PORT, () => {
+  console.log(`Application is listening at port ${PORT}`);
+});
